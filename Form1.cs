91bf646912cd78d0cd0e909d3b1a4b8b3ae72052
Bahattin614 <ayten_baha@hotmail.com>
2023-04-06 07:41:46 +0300

    Hafta 5 - 6 update

--- Form1.cs
+++ Form1.cs
@@ -19,14 +19,66 @@ namespace hafta4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int i, b;
-            for (i = 1; i <= 100; i++) 
+            int i, a, b, c, sum = 0;
+
+            /*
+            for (i = 0; i <= 100; i++)
             {
                 listBox1.Items.Add(i);
+            }
+            */
+                        
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Girmediginiz deger bulunuyor lutfen giriniz!!!");
+            }
+
+            else
+            {
+                a = Convert.ToInt32(textBox1.Text);
+                b = Convert.ToInt32(textBox2.Text);
+                c = Convert.ToInt32(textBox3.Text);
+
+                if (b > a)
+                {
+                    for (i = a; i <= b; i += c)
+                    {
+                        sum = sum + i;
+                        listBox1.Items.Add(i);
+                    }
+
+                    textBox4.Text = sum.ToString();
+                }
+
+                else
+                {
+                    for (i = b; i <= a; i += c)
+                    {
+                        sum = sum + i;
+                        listBox1.Items.Add(i);
+                    }
+
+                    textBox4.Text = sum.ToString();
+                }
             }
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("deneme");
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
